a3a146bfecba39f60f4b53d3bc6e16939a347dac
SashaKuspys <134163412+SashaKuspys@users.noreply.github.com>
2024-01-12 10:54:13 +0200

    change in structure

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from src.conf.config import config as app_config
-from src.database.models import Base
+from src.entity.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
