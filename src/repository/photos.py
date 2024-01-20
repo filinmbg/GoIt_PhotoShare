257672b118b768b9ac431cbb0370e877c4412fd2
dizir7772 <v.a.artemenko1990@gmail.com>
2024-01-20 18:34:37 +0200

    add photo update, get, delete

--- src/repository/photos.py
+++ src/repository/photos.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
 from src.entity.models import User, Image
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.conf.config import config
@@ -5,8 +8,9 @@ from cloudinary.uploader import upload
 from src.repository.qrcode_generator import generate_qr_code
 from src.services.auth_service import get_current_user
 import cloudinary.uploader
+from cloudinary.uploader import destroy
 import cloudinary.api
-from fastapi import UploadFile, File, Form
+from fastapi import UploadFile, File, Form, HTTPException
 import uuid
 
 
@@ -19,6 +23,7 @@ class Cloudinary:
     )
 
 
+# створення світлини
 async def create_image(db: AsyncSession, file: UploadFile = File(), text: str = Form(...), user: User = None):
     img_content = await file.read()
     public_id = f"image_{user.id}_{uuid.uuid4()}"
@@ -32,13 +37,14 @@ async def create_image(db: AsyncSession, file: UploadFile = File(), text: str = 
     qr_code_content = generate_qr_code(response["secure_url"])
     qr_public_id = f"qrcode_{user.id}_{uuid.uuid4()}"
     qr_response = cloudinary.uploader.upload(
-        qr_code_content, public_id=qr_public_id, overwrite=True, folder="qrcodes"
+        qr_code_content, overwrite=True, folder="qrcodes"
     )
 
     # Зберігання в базі даних
     image = Image(
         user_id=user.id,
         url=response["secure_url"],
+        public_id=public_id,
         description=text,
         qr_url=qr_response["secure_url"],
     )
@@ -48,3 +54,45 @@ async def create_image(db: AsyncSession, file: UploadFile = File(), text: str = 
     await db.refresh(image)
 
     return image
+
+
+# отримувати світлину за унікальним посиланням в БД
+async def get_image(image_id: int, session: AsyncSession):
+    image = await session.execute(select(Image).filter(Image.id == image_id))
+    image = image.scalar()
+    if not image:
+        raise HTTPException(status_code=404, detail="Image not found")
+    return image
+
+
+async def update_image(image_id: int, new_description: str, db: Session):
+    image = await db.execute(select(Image).filter(Image.id == image_id))
+    image = image.scalar()
+
+    if not image:
+        raise HTTPException(status_code=404, detail="Image not found")
+
+    image.description = new_description
+    await db.commit()
+    await db.refresh(image)
+
+    return image
+
+
+async def delete_image(image_id: int, db: AsyncSession, user: User = None):
+    image = await db.execute(select(Image).filter(Image.id == image_id))
+    image = image.scalar()
+    if not image:
+        raise HTTPException(status_code=404, detail="Image not found")
+    if image.user_id != user.id:
+        raise HTTPException(status_code=403, detail="Permission denied")
+    try:
+        destroy_result = destroy(public_id=image.public_id, resource_type="image")
+        print("Cloudinary Destroy Result:", destroy_result)
+    except Exception as e:
+        print("Error during Cloudinary destroy:", str(e))
+
+    await db.delete(image)
+    await db.commit()
+
+    return {"message": "Image deleted successfully"}
--- src/routes/photo_routes.py
+++ src/routes/photo_routes.py
@@ -1,18 +1,26 @@
 from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File, Form
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 from src.database.db import get_db
-from src.schemas.photo_schemas import PostSingle
+from src.schemas.photo_schemas import PostSingle, GetSingle, PutSingle, DeleteSingle
 from src.services.auth_service import get_current_user
-from src.repository.photos import create_image
-from src.entity.models import User
-
+from src.repository.photos import create_image, get_image, update_image, delete_image
+from src.entity.models import User, Role
+from src.services.role_service import RoleAccess
 
 router = APIRouter(prefix='/photos', tags=['photos'])
+
+allowed_operation_admin = RoleAccess([Role.admin])
+allowed_operation_create = RoleAccess([Role.admin, Role.moderator, Role.user])
+allowed_operation_read = RoleAccess([Role.admin, Role.moderator, Role.user])
+allowed_operation_update = RoleAccess([Role.admin, Role.moderator, Role.user])
+allowed_operation_delete = RoleAccess([Role.admin, Role.moderator, Role.user])
 
 
 @router.post("/",
              response_model=PostSingle,
-             status_code=status.HTTP_201_CREATED)
+             status_code=status.HTTP_201_CREATED,
+             dependencies=[Depends(allowed_operation_create)])
 async def create_images(
         db: AsyncSession = Depends(get_db),
         file: UploadFile = File(),
@@ -22,5 +30,52 @@ async def create_images(
     try:
         created_image = await create_image(db, file, text, user)
         return created_image
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.get("/{photo_id}",
+            response_model=GetSingle,
+            dependencies=[Depends(allowed_operation_read)])
+async def get_images(
+        image_id: int,
+        session: AsyncSession = Depends(get_db),
+        user: User = Depends(get_current_user)
+):
+    try:
+        image = await get_image(image_id, session)
+        return image
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.put("/{photo_id}",
+            response_model=PutSingle,
+            dependencies=[Depends(allowed_operation_update)])
+async def update_images(
+        image_id: int,
+        text: str,
+        db: Session = Depends(get_db),
+        user: User = Depends(get_current_user)
+):
+    try:
+        updated_image = await update_image(image_id, text, db)
+        return updated_image
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.delete("/{photo_id}",
+               response_model=DeleteSingle,
+               dependencies=[Depends(allowed_operation_delete)])
+async def delete_images(
+        image_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(get_current_user)
+):
+    try:
+        if user.role in allowed_operation_admin.allowed_roles:
+            deleted_image = await delete_image(image_id, db, user)
+            return deleted_image
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
